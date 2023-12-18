857232f2fd3982ba0d1cffc06442efbdc65218d1
Paul Dec <paul.decrosse@gmail.com>
2023-12-18 23:34:21 +0100

    fix

--- components/row-visuel.tsx
+++ components/row-visuel.tsx
@@ -4,36 +4,36 @@ export default async function RowVisuel() {
   return (
     <div className="flex w-full overflow-hidden py-4">
       <div className="flex flex-row gap-10 animate-infinite-scroll">
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/1.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/2.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/3.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/4.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/5.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/6.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/7.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/8.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/9.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/10.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/11.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/12.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/13.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/14.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/15.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/1.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/2.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/3.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/4.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/5.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/6.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/7.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/8.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/9.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/10.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/11.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/12.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/13.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/14.png" alt="logo" />
-        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/visuel/15.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/1.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/2.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/3.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/4.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/5.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/6.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/7.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/8.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/9.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/10.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/11.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/12.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/13.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/14.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/15.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/1.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/2.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/3.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/4.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/5.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/6.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/7.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/8.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/9.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/10.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/11.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/12.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/13.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/14.png" alt="logo" />
+        <Image width={500} height={500} className="flex w-96 h-96 rounded-lg shadow-white-img" src="/Visuel/15.png" alt="logo" />
       </div>
     </div>
   )
