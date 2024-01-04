c6119a995a46ec2dcf08ad279a577016e1411998
Paul Dec <paul.decrosse@gmail.com>
2024-01-04 15:04:04 +0100

    fix

--- components/row-ugc.tsx
+++ components/row-ugc.tsx
@@ -27,32 +27,8 @@ export default async function RowUGC() {
         <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
           <source src="/UGC/4.mp4" type="video/mp4" />
         </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/1.mp4" type="video/mp4" />
-        </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/2.mp4" type="video/mp4" />
-        </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/3.mp4" type="video/mp4" />
-        </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/4.mp4" type="video/mp4" />
-        </video>
       </div>
       <div className="flex flex-row gap-10 animate-infinite-scroll">
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/5.mp4" type="video/mp4" />
-        </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/6.mp4" type="video/mp4" />
-        </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/7.mp4" type="video/mp4" />
-        </video>
-        <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
-          <source src="/UGC/8.mp4" type="video/mp4" />
-        </video>
         <video width="500" height="500" className="flex w-96 h-96 rounded-lg" loop autoPlay muted>
           <source src="/UGC/5.mp4" type="video/mp4" />
         </video>
