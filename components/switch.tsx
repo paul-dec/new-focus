2ee6e70d7b1f6e07d6e613644affd5307b0007e3
Paul Dec <paul.decrosse@gmail.com>
2024-03-04 15:52:36 +0100

    update text

--- components/switch.tsx
+++ components/switch.tsx
@@ -10,9 +10,9 @@ export default async function Switch() {
     <div className="flex flex-col justify-center items-center gap-20 py-24 lg:py-36 max-w-5xl">
       <div className="flex flex-col sm:flex-row gap-20 xl:translate-x-10">
         <div className="flex flex-col gap-4">
-          <h3 className="text-6xl font-black">Motion</h3>
-          <p className="text-2xl font-bold font-roboto">Des contenus sur-mesure pour répondre à vos objectifs médias</p>
-          <p className="font-medium font-roboto">Nos équipes réalisent pour vous des tournages sur-mesure afin de produire des snack content performants. UGC, unboxing, haul, reportages, sketchs, nous analysons ce qui marche sur le web et l&apos;adaptons pour votre audience.</p>
+          <h3 className="text-6xl font-black">Motion design</h3>
+          <p className="text-2xl font-bold font-roboto">Des animations sur mesure et adaptées à votre image de marque</p>
+          <p className="font-medium font-roboto">En partant de vos ressources, nous créons des vidéos personnalisées conçues pour exceller dans vos campagnes médias.</p>
           <a href="https://cal.com/robin-focus/offre-illimitee" target="_blank" className="flex w-fit mt-4 px-8 py-4 bg-white rounded-lg text-black font-bold hover:scale-105 transition-transform uppercase">En savoir plus</a>
         </div>
         <video width="500" height="500" className="flex w-96 h-auto rounded-lg" loop autoPlay muted>
@@ -24,9 +24,9 @@ export default async function Switch() {
           <source src="/vid_ugc.mp4" type="video/mp4" />
         </video>
         <div className="flex flex-col gap-4">
-          <h3 className="text-6xl font-black">UGC</h3>
-          <p className="text-2xl font-bold font-roboto">Des contenus sur-mesure pour répondre à vos objectifs médias</p>
-          <p className="font-medium font-roboto">Nos équipes réalisent pour vous des tournages sur-mesure afin de produire des snack content performants. UGC, unboxing, haul, reportages, sketchs, nous analysons ce qui marche sur le web et l&apos;adaptons pour votre audience.</p>
+          <h3 className="text-6xl font-black">Acting</h3>
+          <p className="text-2xl font-bold font-roboto">Des vidéos avec acteurs pour faire exploser vos performances</p>
+          <p className="font-medium font-roboto">Pour un rendu optimal, nous activons des acteurs qui ont l&apos;habitude de réaliser des vidéos publicitaires dédiées aux réseaux sociaux (UGC, Unboxing, Haul, Trends TikTok)</p>
           <a href="https://cal.com/robin-focus/offre-illimitee" target="_blank" className="flex w-fit mt-4 px-8 py-4 bg-white rounded-lg text-black font-bold hover:scale-105 transition-transform uppercase">En savoir plus</a>
         </div>
       </div>
