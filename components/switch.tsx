946c7262eac66c0c9bcefb0c47efb3faa2109e37
Paul Dec <paul.decrosse@gmail.com>
2024-03-05 14:14:36 +0100

    fix

--- components/switch.tsx
+++ components/switch.tsx
@@ -1,10 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import RowUGC from "./row-ugc";
-import RowMotion from "./row-motion";
-import RowVisuel from "./row-visuel";
-
 export default async function Switch({text1, text2, text3, text4, text5, text6, text7, text8}: {text1: string, text2: string, text3: string, text4: string, text5: string, text6: string, text7: string, text8: string}) {
   return (
     <div className="flex flex-col justify-center items-center gap-20 py-24 lg:py-36 max-w-5xl">
@@ -15,12 +8,12 @@ export default async function Switch({text1, text2, text3, text4, text5, text6, 
           <p className="font-medium font-roboto">{text3}</p>
           <a href="https://cal.com/robin-focus/offre-illimitee" target="_blank" className="flex w-fit mt-4 px-8 py-4 bg-white rounded-lg text-black font-bold hover:scale-105 transition-transform uppercase">{text4}</a>
         </div>
-        <video width="500" height="500" className="flex w-96 h-auto rounded-lg" loop autoPlay muted>
+        <video width="500" height="500" className="flex w-96 h-auto rounded-lg" playsInline preload="auto" loop autoPlay muted>
           <source src="https://res.cloudinary.com/dgsxyadyt/video/upload/v1709635167/ewypqgqyjk4uwzyulurb.mp4" type="video/mp4" />
         </video>
       </div>
       <div className="flex flex-col-reverse sm:flex-row gap-20 xl:-translate-x-10">
-        <video width="500" height="500" className="flex w-96 h-auto rounded-lg" loop autoPlay muted>
+        <video width="500" height="500" className="flex w-96 h-auto rounded-lg" playsInline preload="auto" loop autoPlay muted>
           <source src="https://res.cloudinary.com/dgsxyadyt/video/upload/v1709635167/moyczayhzxelniihvgua.mp4" type="video/mp4" />
         </video>
         <div className="flex flex-col gap-4">
