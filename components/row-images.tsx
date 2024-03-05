eb7be931594a7c3a8e13fbfdf3a9cd8118b89239
Paul Dec <paul.decrosse@gmail.com>
2024-03-05 12:18:58 +0100

    fix

--- components/row-images.tsx
+++ components/row-images.tsx
@@ -6,26 +6,26 @@ export default async function Rowimgs() {
       <div className="flex flex-row gap-10 animate-infinite-scroll-mobile lg:animate-infinite-scroll">
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/1.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/2.png" alt="logo" />
-        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" loop autoPlay muted>
-          <source src="https://res.cloudinary.com/dgsxyadyt/video/upload/v1709635012/a1ghb0f4q3usakwa0fjm.mp4" type="video/mp4" />
+        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" playsInline preload="auto" loop autoPlay muted>
+          <source src="/Motion/1.mp4" type="video/mp4" />
         </video>
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/4.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/5.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/6.png" alt="logo" />
-        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" loop autoPlay muted>
-          <source src="https://res.cloudinary.com/dgsxyadyt/video/upload/v1709635005/ykh0gpxyd09iwybgoqhn.mp4" type="video/mp4" />
+        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" playsInline preload="auto" loop autoPlay muted>
+          <source src="/UGC/1.mp4" type="video/mp4" />
         </video>
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/7.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/1.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/2.png" alt="logo" />
-        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" loop autoPlay muted>
-          <source src="https://res.cloudinary.com/dgsxyadyt/video/upload/v1709635012/a1ghb0f4q3usakwa0fjm.mp4" type="video/mp4" />
+        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" playsInline preload="auto" loop autoPlay muted>
+          <source src="/Motion/1.mp4" type="video/mp4" />
         </video>
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/4.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/5.png" alt="logo" />
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/6.png" alt="logo" />
-        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" loop autoPlay muted>
-          <source src="https://res.cloudinary.com/dgsxyadyt/video/upload/v1709635005/ykh0gpxyd09iwybgoqhn.mp4" type="video/mp4" />
+        <video width="500" height="500" className="flex w-fit h-64 lg:h-96 rounded-lg" playsInline preload="auto" loop autoPlay muted>
+          <source src="/UGC/1.mp4" type="video/mp4" />
         </video>
         <img className="flex w-fit h-64 lg:h-96 rounded-lg" src="/Visuel/7.png" alt="logo" />
       </div>
