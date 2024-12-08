4dfeaee116ba355828e2b6206c17e95e61a02aab
robinwearefocus <163005229+robinwearefocus@users.noreply.github.com>
2024-12-08 16:43:36 +0100

    updatetext

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-4 lg:p-10">
       <Navbar text1='Comment ça marche&nbsp;?' text2='Tarification' text3='FAQ' text4='Cliquer pour réserver un appel' language='🇺🇸 EN' />
-      <Header text1='L’agence créative' text2='par ' text3='abonnement' text4='Du contenu' text5='performant et adapté à vos besoins' text6='en moins de' text7='48h.' text8='Pause ou annulation à tout moment.'text9='Voir les abonnements' />
+      <Header text1='L’agence créative' text2='par ' text3='abonnement' text4='Du contenu' text5='performant et adapté à vos besoins' text6='en moins de' text7='48h.' text8='Pause ou annulation à tout moment.'text9='Nos abonnements' />
       <div className='mb-24 lg:mb-36'>
         <RowImages />
       </div>
@@ -23,7 +23,7 @@ export default function Home() {
       <Pricing text1='Tarification' text2='Utilisez notre service pour produire du contenu en masse.' text3='Basic' text4='4900€/mois' text5='Pause ou annulation à tout moment' text6='2 requêtes à la fois' text7='Inclus :' text8='Graphisme / Montage / Motion Design en illimité' text9='Deux requêtes à la fois' text10='Temps moyen de traitement de 72h' text11='Retours illimités' text12='Nombre d’utilisateurs et de marques illimités' text13='Pause ou arrêt de l’abonnement à tout moment' text14='Réserver un appel' text15='Pro' text16='7900€/mois' text17='Pause ou annulation à tout moment' text18='3 requêtes (avec de l’UGC)' text19='Inclus :' text20='Graphisme / Montage / Motion Design / UGC en illimité' text21='Trois requêtes à la fois' text22='Temps moyen de traitement de 72h' text23='Retours illimités' text24='Nombre d’utilisateurs et de marques illimités' text25='Pause ou arrêt de l’abonnement à tout moment' text26='Réserver un appel' />
       <Faq text1='FAQ' text2='Quand vais-je recevoir mes contenus&nbsp;?' text3='Nous traitons en moyenne toutes les demandes en 72h en vous tenant informé. Pour les demandes plus complexes comme les UGC, les délais sont légèrement allongés pour permettre à l’acteur de tourner dans de bonnes conditions.' text4='Comment faire une requête pour un nouveau contenu&nbsp;?' text5='Nous utilisons Notion pour traiter vos requêtes. Vous êtes libre d’écrire votre brief directement sur Notion ou de nous transmettre un Loom, un Google Doc, ou n’importe quel support qui nous aide à comprendre vos idées.' text6='Pourquoi mettre en pause mon abonnement&nbsp;?' text7='Tous les mois ne se ressemblent pas pour votre activité : nous avons mis en place la possibilité de mettre en pause votre abonnement et de le réactiver à tout moment.' text8='Pourquoi ne pas embaucher une équipe créative à temps plein&nbsp;?' text9='Nous mettons à votre disposition toute notre expertise en graphisme, montage vidéo, motion design, ainsi que plus de 100 acteurs. L&apos;embauche d&apos;une équipe créative complète (un graphiste, un motion designer, un chef de projet…) sera plus coûteuse et difficile à gérer : nous nous occupons de tout cela pour vous !' />
       <Start text1='Réserver un appel' text2='20mn pour rencontrer nos équipes' />
-      <BottomBar text1='Agence créative à Nice' text2='© 2023 Focus' text3='Mentions légales' />
+      <BottomBar text1='Agence créative basée à Nice' text2='© 2025 Focus' text3='Mentions légales' />
     </main>
   )
 }
